9b7912c3e8272ab417d116994a65140d50800057
Florence Tan <Florence.Tan@csiro.au>
2014-05-02 07:52:46 +0000

    Move swift and scipy from pip packages to default packages - they are now available in the epel repo.

--- vm/puppet/modules/vgl_common/manifests/init.pp
+++ vm/puppet/modules/vgl_common/manifests/init.pp
@@ -4,23 +4,17 @@
 class vgl_common {
 
     # Install default packages
-    package { ["wget", "subversion", "mercurial", "ftp", "bzip2", "elfutils", "ntp", "ntpdate", "gcc", "gcc-c++", "make", "openssh", "openssh-clients", "swig", "libpng-devel", "freetype-devel", "atlas", "atlas-devel", "libffi-devel"]: 
+    package { ["wget", "subversion", "mercurial", "ftp", "bzip2", "elfutils", "ntp", "ntpdate", "gcc", "gcc-c++", "make", "openssh", "openssh-clients", "swig", "libpng-devel", "freetype-devel", "atlas", "atlas-devel", "libffi-devel", "swift", "scipy"]: 
         ensure => installed,
         require => Class["epel"],
     }
     
     # Install default pip packages
-    package {  ["numpy", "boto", "pyproj", "swift", "python-keystoneclient"]:
+    package {  ["numpy", "boto", "pyproj", "python-keystoneclient"]:
         ensure => installed,
         provider => "pip",
         require => Class["python_pip"],
     }  
-    package {["scipy"]:
-        ensure => installed,
-        provider => "pip",
-        require => [Class["python_pip"], Package["numpy"]],
-    }
-
     
     
     # Install startup bootstrap
