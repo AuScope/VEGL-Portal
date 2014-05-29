8cc096bcbcfb906d8463903408be153832c42b9d
Terry Rankine <Terry.Rankine@csiro.au>
2014-05-29 07:39:11 +0000

    'string ${::this}' is not SUBSTITUTED - it is passed out to the shell.... if i want to use a variable i need to use "this"

--- vm/puppet/escript.pp
+++ vm/puppet/escript.pp
@@ -25,47 +25,47 @@ class {"escript_packages": }
 
 class escript_deps {
 	# Install cartographic projection library
-	puppi::netinstall { 'proj':
-		url => 'http://download.osgeo.org/proj/proj-4.8.0.tar.gz',
-		extracted_dir => 'proj-4.8.0',
-		destination_dir => '/tmp',
-		postextract_command => '/tmp/proj-4.8.0/configure && make -j${::procplus} && make install',
+	puppi::netinstall { "proj":
+		url => "http://download.osgeo.org/proj/proj-4.8.0.tar.gz",
+		extracted_dir => "proj-4.8.0",
+		destination_dir => "/tmp",
+		postextract_command => "/tmp/proj-4.8.0/configure && make -j${::procplus} && make install",
 		require => [Class["escript_packages"], Class["vgl_common"]],
 	}
 
 	# Install gmsh
-	puppi::netinstall { 'gmsh':
-		url => 'http://geuz.org/gmsh/bin/Linux/gmsh-2.8.4-Linux64.tgz',
-		extracted_dir => 'gmsh-2.8.4-Linux',
-		destination_dir => '/tmp',
-		postextract_command => 'mv /tmp/gmsh-2.8.4-Linux/bin/gmsh /usr/local/bin/',
+	puppi::netinstall { "gmsh":
+		url => "http://geuz.org/gmsh/bin/Linux/gmsh-2.8.4-Linux64.tgz",
+		extracted_dir => "gmsh-2.8.4-Linux",
+		destination_dir => "/tmp",
+		postextract_command => "mv /tmp/gmsh-2.8.4-Linux/bin/gmsh /usr/local/bin/",
 		require => [Class["escript_packages"], Class["vgl_common"]],
 	}
 
 	# Install SILO
-	puppi::netinstall { 'silo':
-		url => 'https://wci.llnl.gov/codes/silo/silo-4.8/silo-4.8-bsd.tar.gz',
-		extracted_dir => 'silo-4.8-bsd',
-		destination_dir => '/tmp',
-		postextract_command => '/tmp/silo-4.8-bsd/configure --prefix=/usr/local && make -j${::procplus} && make install',
+	puppi::netinstall { "silo":
+		url => "https://wci.llnl.gov/codes/silo/silo-4.8/silo-4.8-bsd.tar.gz",
+		extracted_dir => "silo-4.8-bsd",
+		destination_dir => "/tmp",
+		postextract_command => "/tmp/silo-4.8-bsd/configure --prefix=/usr/local && make -j${::procplus} && make install",
 		require => [Class["escript_packages"], Package["gcc-gfortran"], Class["vgl_common"]],
 	}
 
 	# Install SymPy
-	puppi::netinstall { 'sympy':
-		url => 'https://github.com/sympy/sympy/releases/download/sympy-0.7.5/sympy-0.7.5.tar.gz',
-		extracted_dir => 'sympy-0.7.5',
-		destination_dir => '/tmp',
-		postextract_command => 'python /tmp/sympy-0.7.5/setup.py install',
+	puppi::netinstall { "sympy":
+		url => "https://github.com/sympy/sympy/releases/download/sympy-0.7.5/sympy-0.7.5.tar.gz",
+		extracted_dir => "sympy-0.7.5",
+		destination_dir => "/tmp",
+		postextract_command => "python /tmp/sympy-0.7.5/setup.py install",
 		require => [Class["escript_packages"], Class["vgl_common"]],
 	}
 
 	# Install boost
-	puppi::netinstall { 'boost':
-		url => 'http://downloads.sourceforge.net/boost/boost_1_55_0.tar.gz',
-		extracted_dir => 'boost_1_55_0',
-		destination_dir => '/tmp',
-		postextract_command => 'chmod a+x bootstrap.sh && ./bootstrap.sh',
+	puppi::netinstall { "boost":
+		url => "http://downloads.sourceforge.net/boost/boost_1_55_0.tar.gz",
+		extracted_dir => "boost_1_55_0",
+		destination_dir => "/tmp",
+		postextract_command => "chmod a+x bootstrap.sh && ./bootstrap.sh",
 		require => [Class["escript_packages"], Class["vgl_common"]],
 	}
 	
@@ -88,11 +88,11 @@ class {"visit": }
 # Parts of escript require the I/O functionality and will not work. A bug was filed with CentOS who will 
 # hopefully fix the issue in an updated package (see http://bugs.centos.org/view.php?id=5931). 
 # When that bug is fixed you should be able run yum install openmpi but until that time you will need to build from source: 
-puppi::netinstall { 'openmpi':
-    url => 'http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz',
-    extracted_dir => 'openmpi-1.6.5',
-    destination_dir => '/tmp',
-    postextract_command => '/tmp/openmpi-1.6.5/configure --prefix=/usr/local && make -j${procplus} all && make all install',
+puppi::netinstall { "openmpi":
+    url => "http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz",
+    extracted_dir => "openmpi-1.6.5",
+    destination_dir => "/tmp",
+    postextract_command => "/tmp/openmpi-1.6.5/configure --prefix=/usr/local && make -j${::procplus} all && make all install",
     require => [Class["escript_packages"], Class["vgl_common"]],
 }
 
@@ -124,7 +124,7 @@ exec { "escript-config":
 }
 exec { "escript-install":
     cwd => "/tmp/escript-3.4.2",
-    command => "/usr/bin/scons -j${procplus}",
+    command => "/usr/bin/scons -j${::procplus}",
     require => Exec["escript-config"],
     timeout => 0,
 }
