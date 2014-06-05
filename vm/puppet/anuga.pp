63e334b765cd363de6df351c463be90336733597
Terry Rankine <Terry.Rankine@csiro.au>
2014-06-05 05:53:36 +0000

    * certificate HACK * Installers dont seem to find mpicc -> something is not propagating to popen -> hardcoded MPICC path (evil - but this is the quality of researchers code...) * set pythonpath for ANUGA. test "python -c 'import anuga; print anuga.__version__'"

--- vm/puppet/anuga.pp
+++ vm/puppet/anuga.pp
@@ -14,41 +14,41 @@ class { autofsck:
 }
 
 class mpi {
-	# Note: At the time of writing the current OpenMPI package (openmpi-devel-1.5.4-1.el6.x86_64) is missing the necessary I/O component. 
-	# Parts of escript require the I/O functionality and will not work. A bug was filed with CentOS who will 
-	# hopefully fix the issue in an updated package (see http://bugs.centos.org/view.php?id=5931). 
-	# When that bug is fixed you should be able run yum install openmpi but until that time you will need to build from source: 
-	puppi::netinstall { "openmpi":
-		url => "http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz",
-		extracted_dir => "openmpi-1.6.5",
-		destination_dir => "/tmp",
-		postextract_command => "/tmp/openmpi-1.6.5/configure --prefix=/usr/local && make -j${::procplus} all && make all install",
-		require => [Class["vgl_common"]],
-	}
+    # Note: At the time of writing the current OpenMPI package (openmpi-devel-1.5.4-1.el6.x86_64) is missing the necessary I/O component. 
+    # Parts of escript require the I/O functionality and will not work. A bug was filed with CentOS who will 
+    # hopefully fix the issue in an updated package (see http://bugs.centos.org/view.php?id=5931). 
+    # When that bug is fixed you should be able run yum install openmpi but until that time you will need to build from source: 
+    puppi::netinstall { "openmpi":
+        url => "http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz",
+        extracted_dir => "openmpi-1.6.5",
+        destination_dir => "/tmp",
+        postextract_command => "/tmp/openmpi-1.6.5/configure --prefix=/usr/local && make -j${::procplus} all && make all install",
+        require => [Class["vgl_common"]],
+    }
 
-	$mpiShContent= '# Environment for MPI
-	export PATH=/usr/local/bin:$PATH
-	export LD_LIBRARY_PATH=/usr/local/lib/openmpi:/usr/local/lib/:$LD_LIBRARY_PATH'
-	file {"mpi-profile-env":
-		path => "/etc/profile.d/mpi.sh",
-		ensure => present,
-		content => $mpiShContent,
-		require => Puppi::Netinstall['openmpi'],
-	}
+    $mpiShContent= '# Environment for MPI
+    export PATH=/usr/local/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/lib/openmpi:/usr/local/lib/:$LD_LIBRARY_PATH'
+    file {"mpi-profile-env":
+        path => "/etc/profile.d/mpi.sh",
+        ensure => present,
+        content => $mpiShContent,
+        require => Puppi::Netinstall['openmpi'],
+    }
 }
 
 
 class scientificpython {
-	package { ["netcdf-devel"]:
-		ensure => installed,
-		require => Class["epel"],
-	}
-	
+    package { ["netcdf-devel"]:
+        ensure => installed,
+        require => Class["epel"],
+    }
+    
     puppi::netinstall { "scientificpython-inst":
         url => "https://sourcesup.renater.fr/frs/download.php/4425/ScientificPython-2.9.3.tar.gz",
-		extracted_dir => "ScientificPython-2.9.3",
-		destination_dir => "/tmp",		
-		postextract_command => "python setup.py install",
+        extracted_dir => "ScientificPython-2.9.3",
+        destination_dir => "/tmp",        
+        postextract_command => "python setup.py install",
         require => [Class["mpi"], Class["vgl_common"],],
     }
 }
@@ -64,13 +64,13 @@ class aem_packages {
 class pypar {
     puppi::netinstall { "pypar-inst":
         url => "https://pypar.googlecode.com/files/pypar-2.1.5_108.tgz",
-		destination_dir => "/tmp",
-                extracted_dir => "pypar_2.1.5_108/source",
-                postextract_command => "/usr/bin/python /tmp/pypar_2.1.5_108/source/setup.py install",
+        destination_dir => "/tmp",
+        extracted_dir => "pypar_2.1.5_108/source",
+        postextract_command => "perl -pi -e 's#mpicc -show#/usr/local/bin/mpicc -show#g' /tmp/pypar_2.1.5_108/source/setup.py && /usr/bin/python /tmp/pypar_2.1.5_108/source/setup.py install",
         require => [Class["mpi"], Class["vgl_common"],],
     }
 }
-	
+    
 
 class {"mpi":}
 class {"aem_packages": }
@@ -78,20 +78,30 @@ class {"scientificpython": }
 class {"pypar": }
 
 
-#Checkout, configure and install anuga
+#Checkout, configure and install anuga with a 'dodgy SSL cert'
 exec { "anuga-co":
     cwd => "/usr/local",
-    command => "/usr/bin/svn co https://anuga.anu.edu.au/svn/anuga/trunk/anuga_core/ anuga",
+    command => "/bin/echo p | svn info https://anuga.anu.edu.au/svn/anuga/trunk/anuga_core/ && /usr/bin/svn co --trust-server-cert --non-interactive https://anuga.anu.edu.au/svn/anuga/trunk/anuga_core/ anuga",
     creates => "/usr/local/anuga",
     require => [Class["aem_packages"],Class["mpi"], Class["vgl_common"]],
     timeout => 0,
 }
 
-#export PYTHONPATH=/home/******/anuga_core/source
 
+#hack setup file to find mpicc
 exec { "anuga-install":
     cwd => "/usr/local/anuga",
-    command => "/usr/bin/python compile_all.py",
+    command => "perl -pi -e 's#mpicc -show#/usr/local/bin/mpicc -show#g' /usr/local/anuga/source/anuga_parallel/pypar_extras/setup.py && /usr/bin/python compile_all.py",
     require => Exec["anuga-co"],
     timeout => 0,
+}
+
+$anugaenvContent= '# Environment for ANUGA
+export PYTHONPATH="$PYTHONPATH:/usr/local/anuga/source"
+'
+
+file {"anuga-profile-env":
+	path => "/etc/profile.d/anuga.sh",
+	ensure => present,
+	content => $anugaenvContent,
 }
