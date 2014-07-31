26f8121728d2de6611539e2bfe20e3b6b4914590
Geoff Squire <geoffrey.squire@gmail.com>
2014-07-31 16:07:22 +1000

    Pull puppet modules from cloned git repo

--- vm/puppet/installPuppet-centos.sh
+++ vm/puppet/installPuppet-centos.sh
@@ -39,9 +39,9 @@ then
     exit 1
 fi
 
-# VHIRL Portal Custom Modules - download from user specified SVN (or default)
+# VHIRL Portal Custom Modules - download from user specified GIT (or default)
 yum install -y wget
-baseUrl="https://github.com/AuScope/VHIRL-Portal/raw/master"
+baseUrl="https://github.com/AuScope/VHIRL-Portal.git"
 pathSuffix="/vm/puppet/modules/"
 tmpModulesDir="/tmp/modules/"
 rm -rf "$tmpModulesDir"
@@ -54,11 +54,11 @@ then
     pathSuffix="$2"
 fi
 
-#Ensure base url ends with a '/'
-if [ `tail -c 2 <<< "$baseUrl"` != "/" ]
-then
-    baseUrl="$baseUrl/"
-fi
+# #Ensure base url ends with a '/'
+# if [ `tail -c 2 <<< "$baseUrl"` != "/" ]
+# then
+#     baseUrl="$baseUrl/"
+# fi
 
 #Ensure suffix doesn't start with a '/'
 if [ `head -c 2 <<< "$pathSuffix"` != "/" ]
@@ -66,21 +66,26 @@ then
     pathSuffix=`tail -c +2 <<< "$pathSuffix"`
 fi
 
-#When we download we don't want a long tree of directories, we just want the modules directory. Therefore we need to cut directories
-#back by the number of slashes in the URL. Don't forget there will be a trailing '/' and a http://
-baseUrlSlashes=`grep -o "/" <<<"$baseUrl" | wc -l`
-pathSuffixSlashes=`grep -o "/" <<<"$pathSuffix" | wc -l`
-cutDirs=`expr $baseUrlSlashes - 3 + $pathSuffixSlashes`
-wget -r "$baseUrl$pathSuffix" -P "$tmpModulesDir" -R htm,html -nH -np --cut-dirs $cutDirs -l 50
-if [ $? -ne 0 ]
-then
-    echo "Failed download of VGL custom modules - aborting"
-    exit 1
-fi
+# Clone the git repository into $tmpModulesDir so we can extract the
+# puppet modules
+mkdir -p "$tmpModulesDir"
+git clone "$baseUrl" "$tmpModulesDir"
+
+# #When we download we don't want a long tree of directories, we just want the modules directory. Therefore we need to cut directories
+# #back by the number of slashes in the URL. Don't forget there will be a trailing '/' and a http://
+# baseUrlSlashes=`grep -o "/" <<<"$baseUrl" | wc -l`
+# pathSuffixSlashes=`grep -o "/" <<<"$pathSuffix" | wc -l`
+# cutDirs=`expr $baseUrlSlashes - 3 + $pathSuffixSlashes`
+# wget -r "$baseUrl$pathSuffix" -P "$tmpModulesDir" -R htm,html -nH -np --cut-dirs $cutDirs -l 50
+# if [ $? -ne 0 ]
+# then
+#     echo "Failed download of VGL custom modules - aborting"
+#     exit 1
+# fi
 
 #Now copy the modules to the puppet module install directory
 moduleDir="/etc/puppet/modules"
-find "$tmpModulesDir" -maxdepth 1 -mindepth 1 -type d -exec cp {} -r "$moduleDir" \;
+find "$tmpModulesDir/$pathSuffix" -maxdepth 1 -mindepth 1 -type d -exec cp {} -r "$moduleDir" \;
 if [ $? -ne 0 ]
 then
     echo "Failed copying to puppet module directory - aborting"
