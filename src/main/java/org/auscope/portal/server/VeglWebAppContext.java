ef3b4caa0bb421a069d3d546904a396073f3ecdf
Terry Rankine <terry.rankine@csiro.au>
2014-08-14 11:36:05 +0800

    * renamed config files to be more generic * Updated snapshot ID * updated busselton dataset detector - still not nice....

--- src/main/java/org/auscope/portal/server/VeglWebAppContext.java
+++ src/main/java/org/auscope/portal/server/VeglWebAppContext.java
@@ -15,8 +15,8 @@ public class VeglWebAppContext extends PortalProfileXmlWebApplicationContext {
         String[] locations = super.getDefaultConfigLocations();
 
         String[] auscopeLocations = Arrays.copyOf(locations, locations.length + 3);
-        auscopeLocations[auscopeLocations.length - 1] = DEFAULT_CONFIG_LOCATION_PREFIX + "vegl-known-layers.xml";
-        auscopeLocations[auscopeLocations.length - 2] = DEFAULT_CONFIG_LOCATION_PREFIX + "vegl-registries.xml";
+        auscopeLocations[auscopeLocations.length - 1] = DEFAULT_CONFIG_LOCATION_PREFIX + "vl-known-layers.xml";
+        auscopeLocations[auscopeLocations.length - 2] = DEFAULT_CONFIG_LOCATION_PREFIX + "vl-registries.xml";
         auscopeLocations[auscopeLocations.length - 3] = DEFAULT_CONFIG_LOCATION_PREFIX + "applicationContext-security.xml";
         return auscopeLocations;
     }
